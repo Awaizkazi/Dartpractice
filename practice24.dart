604810d307b019c4808e08136accbb522b78a3e6
Awaizkazi <88947135+Awaizkazi@users.noreply.github.com>
2022-02-25 21:35:20 +0530

    commit

---
+++ practice24.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  print('**** WELCOME TO MICHEAL KITCHEN');
+  print('How many people you are :');
+
+  int userInput = int.parse(stdin.readLineSync()!);
+  print('');
+}
