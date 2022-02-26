ae4c0344bd32b94c592b4eaaf2f3d7166c00a359
Awaizkazi <88947135+Awaizkazi@users.noreply.github.com>
2022-02-26 14:42:04 +0530

    commit

--- practice24.dart
+++ practice24.dart
@@ -1,9 +1,20 @@
 import 'dart:io';
 
 void main() {
+  // Easily i calcualte the Statically
+  // print(200 * 10 / 100);
+  int totalBillAmount, totalTip, totalAmount;
+  // WELCOME TO KITCHEN
   print('**** WELCOME TO MICHEAL KITCHEN');
   print('How many people you are :');
-
   int userInput = int.parse(stdin.readLineSync()!);
-  print('');
+  //  Totoal Amount of Bill
+  print('How Much your Totoal Amount is :');
+  totalBillAmount = int.parse(stdin.readLineSync()!);
+
+  print('How many % of tip you Wants to give :');
+  totalTip = int.parse(stdin.readLineSync()!);
+
+  double giveTip = totalBillAmount * totalTip / 100;
+  print('Each person Wants to give this amount $giveTip');
 }
